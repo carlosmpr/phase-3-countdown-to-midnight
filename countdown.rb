9cc274503c0b9651a1274deebdecd88378ef3bb4
Carlos Polanco <carlospolanco@Carloss-Mac-mini.local>
2021-06-28 21:07:20 -0400

    commit tree a54de11a724a5d112912a1e27a9a1f5d1275763e

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
+def countdown_with_sleep
+    sleep(5.seconds)
+end
 
-def countdown
+def countdown(number)
+    number = 10
+    while number > 0
+        puts  "#{number} SECOND(S)!"
+        number -= 1
+        countdown_with_sleep
+    end
+    return "HAPPY NEW YEAR!" 
 end
