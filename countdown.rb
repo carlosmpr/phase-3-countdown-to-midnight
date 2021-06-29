89dcc2aeccdb808050edc511dcd00a28d719d6ab
Carlos Polanco <carlospolanco@Carloss-Mac-mini.local>
2021-06-28 21:09:18 -0400

    completed

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
+def countdown_with_sleep
+    sleep 5
+end
 
-def countdown
+def countdown(number)
+    number = 10
+    while number > 0
+        puts  "#{number} SECOND(S)!"
+        countdown_with_sleep
+        number -= 1
+      
+    end
+    return "HAPPY NEW YEAR!" 
 end
